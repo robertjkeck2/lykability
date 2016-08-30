dcdfffeeda2abc57672be3e8a9673849b417433a
John <robertjkeck2@gmail.com>
2016-08-30 00:49:43 -0400

    Update and rename likability/test_script.py to test/test_script.py

--- likability/test_script.py
+++
@@ -1,6 +0,0 @@
-from LikabilityAnalyzer import LikabilityAnalyzer
-
-a = LikabilityAnalyzer('test_names.csv',100)
-
-a.score()
-
---
+++ test/test_script.py
@@ -0,0 +1,6 @@
+from lykability import LikabilityAnalyzer
+
+a = LikabilityAnalyzer.analyzer('test_names.csv',100)
+
+a.score()
+
